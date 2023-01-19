785012b61baa5cc6ef80e6cb9d0d169c615e6476
Nibras <nibrasmn027@gmail.com>
2023-01-19 12:39:44 +0530

    update

--- go.mod
+++ go.mod
@@ -4,12 +4,11 @@ go 1.19
 
 require (
 	github.com/gofiber/fiber/v2 v2.41.0
-	github.com/gofiber/utils v1.0.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/google/UUID v1.0.0
 	github.com/google/uuid v1.3.0
 	github.com/johnfercher/maroto v0.39.0
 	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/testify v1.7.0
 	github.com/stripe/stripe-go/v74 v74.5.0
 	github.com/twilio/twilio-go v1.3.0
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
@@ -20,6 +19,7 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -30,10 +30,12 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/ruudk/golang-pdf417 v0.0.0-20181029194003-1af4ab5afa58 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.43.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- routes/userroute_test.go
+++ routes/userroute_test.go
@@ -1,32 +1,104 @@
-package routes
+package routes_test
 
 import (
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/nibrasmuhamed/cartique/controllers"
+	"github.com/nibrasmuhamed/cartique/database"
+	"github.com/nibrasmuhamed/cartique/routes"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRouter_Routes(t *testing.T) {
-	type fields struct {
-		UserRoute controllers.UserController
-	}
-	type args struct {
-		user fiber.Router
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		description string
+
+		// Test input
+		route string
+
+		// Expected output
+		expectedError bool
+		expectedCode  int
+		expectedBody  string
 	}{
-		// TODO: Add test cases.
+		{
+			description:   "index route",
+			route:         "/user/login",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  "OK",
+		},
+		{
+			description:   "non existing route",
+			route:         "/i-dont-exist",
+			expectedError: false,
+			expectedCode:  404,
+			expectedBody:  "Cannot GET /i-dont-exist",
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			u := &UserRouter{
-				UserRoute: tt.fields.UserRoute,
-			}
-			u.Routes(tt.args.user)
-		})
+
+	var UC *controllers.UserController
+	var AC *controllers.AdminController
+	var ar *routes.AdminRouter
+	var ur *routes.UserRouter
+	var Pc *controllers.ProductDB
+	var Pr *routes.ProductRouter
+
+	//  := routes.UserRouter
+	app := fiber.New()
+	x := database.InitDB()
+	AC = controllers.NewAdminController(x)
+	UC = controllers.NewUserController(x)
+	Pc = controllers.NewProductDB(x)
+	ur = routes.NewUserRouter(UC)
+	ar = routes.NewAdminRouter(*AC)
+	Pr = routes.NewProductRoter(Pc)
+
+	products := app.Group("/products")
+	Pr.Routes(products)
+	admin := app.Group("/admin")
+	ar.AdminRoute(admin)
+	user := app.Group("/user")
+	ur.Routes(user)
+
+	// Iterate through test single test cases
+	for _, test := range tests {
+		// Create a new http request with the route
+		// from the test case
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+
+		// Perform the request plain with the app.
+		// The -1 disables request latency.
+		res, err := app.Test(req, -1)
+
+		// verify that no error occured, that is not expected
+
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		// As expected errors lead to broken responses, the next
+		// test case needs to be processed
+		// if test.expectedError {
+		// 	continue
+		// }
+
+		// Verify if the status code is as expected
+		// assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		// Read the response body
+		body, err := ioutil.ReadAll(res.Body)
+
+		// Reading the response body should work everytime, such that
+		// the err variable should be nil
+		// assert.Nilf(t, err, test.description)
+
+		// Verify, that the reponse body equals the expected body
+		assert.Equalf(t, test.expectedBody, string(body), test.description)
 	}
 }
