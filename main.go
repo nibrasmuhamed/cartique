40d4a5b452d59d4aa406efe1e7eb13d1e00f193a
Nibras <nibrasmn027@gmail.com>
2023-01-25 19:22:39 +0530

    cicd and home updated

--- main.go
+++ main.go
@@ -34,17 +34,17 @@ func main() {
 
 	app := fiber.New()
 	// app.Use(csrf.New())
-	app.Static("/", "./public")
 	// app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowHeaders:     "*",
 		AllowOrigins:     "*",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
+	app.Static("/static", "./public")
 	app.Get("/", home)
 	products := app.Group("/products")
 	Pr.Routes(products)
